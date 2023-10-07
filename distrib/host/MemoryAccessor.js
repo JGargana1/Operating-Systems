783030928b4601e3023720d038509b3b2eaee698
“Jeremy <jeremy.gargana1@marist.edu>
2023-10-07 18:32:07 -0400

    updates memory segment display on screen

--- distrib/host/MemoryAccessor.js
+++ distrib/host/MemoryAccessor.js
@@ -17,9 +17,21 @@ var TSOS;
         write(address, value) {
             if (address >= 0 && address < this.memory.memoryArray.length) {
                 this.memory.memoryArray[address] = value;
+                this.updateMemoryDisplay(address, value);
             }
             else {
                 _Kernel.krnTrace(`Invalid memory write address: ${address}`);
+            }
+        }
+        updateMemoryDisplay(address, value) {
+            let table = document.getElementById("memoryTable");
+            let rowIndex = Math.floor(address / 8);
+            let cellIndex = (address % 8) + 1; // +1 because of the address cell at the start of each row
+            if (table && table.rows[rowIndex]) {
+                let row = table.rows[rowIndex];
+                if (row.cells[cellIndex]) {
+                    row.cells[cellIndex].textContent = value.toUpperCase(); // Update the value cell, made it uppercase assuming hexadecimal
+                }
             }
         }
     }
--- source/host/MemoryAccessor.ts
+++ source/host/MemoryAccessor.ts
@@ -7,7 +7,6 @@ module TSOS {
             if (address >= 0 && address < this.memory.memoryArray.length) {
                 return this.memory.memoryArray[address];
             } else {
-                
                 _Kernel.krnTrace(`Invalid memory read address: ${address}`);
                 return "00";
             }
@@ -16,10 +15,24 @@ module TSOS {
         public write(address: number, value: string): void {
             if (address >= 0 && address < this.memory.memoryArray.length) {
                 this.memory.memoryArray[address] = value;
+                this.updateMemoryDisplay(address, value);
             } else {
-                
                 _Kernel.krnTrace(`Invalid memory write address: ${address}`);
             }
         }
+
+        private updateMemoryDisplay(address: number, value: string): void {
+            let table: HTMLTableElement = <HTMLTableElement>document.getElementById("memoryTable");
+            let rowIndex = Math.floor(address / 8);
+            let cellIndex = (address % 8) + 1;  
+        
+            if (table && table.rows[rowIndex]) {
+                let row = table.rows[rowIndex];
+                if (row.cells[cellIndex]) {
+                    row.cells[cellIndex].textContent = value.toUpperCase(); 
+                }
+            }
+        }
+        
     }
 }
